870c9dbe0cc24100a4b5ddab7f46440351a566c9
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-03-21 11:30:36 +0100

    structure de base ex02

---
+++ ex02/whois.py
@@ -0,0 +1,10 @@
+#!/Users/porcedduvalentin/miniconda3/envs/42AI-porcedduvalentin/bin python
+import sys
+
+arg = sys.argv
+if (len(arg)==2):
+	print('good !')
+elif (len(arg)==1):
+	0
+else:
+	print('error')
