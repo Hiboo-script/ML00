d028b4cebab2bd5e4e505dad083bd08803bc26a2
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-03-21 11:56:40 +0100

    ex02 fonctionnel

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,14 +1,24 @@
 #!/Users/porcedduvalentin/miniconda3/envs/42AI-porcedduvalentin/bin python
 import sys
 n = 0
+zero = 'I\'m zero.'
+even = 'I\'m Even.'
+odd = 'I\'m Odd.'
+noInt = 'AssertionError : argument is not an integer'
+tooMuchArg = 'AssertionError : more than one argument are provided'
 arg = sys.argv
 if (len(arg)==2):
 	try:
-		number = int(arg[1])
-		print('good')
+		n = int(arg[1])
+		if (n == 0):
+			print(zero)
+		elif (n%2==0):
+			print(even)
+		else:
+			print(odd)
 	except:
-		print('error 2')
+		print(noInt)
 elif (len(arg)==1):
 	0
 else:
-	print('error')
+	print(tooMuchArg)
