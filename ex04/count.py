e8f8d4ce87c8200ee5e1d33f39baf726f2298437
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-04-07 00:39:07 +0200

    ex04 much nicer final code !

--- ex04/count.py
+++ ex04/count.py
@@ -1,33 +1,47 @@
-from string import punctuation as punct
+import string
 
-def analyzer_prompt(u,l,p,s):
-	print("- "+str(u)+" upper letter(s)")
-	print("- "+str(l)+" lower letter(s)")
-	print("- "+str(p)+" punctuation mark(s)")
-	print("- "+str(s)+" space(s)")
+def analyzer_prompt(uppercase_letters,lowercase_letters,punctuation_marks,spaces):
+	print(f"- {uppercase_letters} upper letter(s)")
+	print(f"- {lowercase_letters} lower letter(s)")
+	print(f"- {punctuation_marks} punctuation mark(s)")
+	print(f"- {spaces} space(s)")
 
 def text_analyzer(text = ""):
 	"""
 	This function counts the number of upper characters, 
 	lower characters, punctuation and spaces in a given text.
 	"""
-	if(type(text)!=type("s")):
-		print("insertionError : the argument is not a string.")
+	insertionError = "insertionError : the argument is not a string."
+	if not isinstance(text,str):
+		print(insertionError)
 		return None
-	elif (len(text)==0): 
+	elif not text: 
 		text = input("What is the text to analize?\n>> ")
 	
-	up = 0
-	lo = 0
-	pu = 0
-	sp = 0
-	for c in text:
-		if (c.isupper()):
-			up+=1
-		elif (c.islower()):
-			lo+=1
-		elif (c in punct):
-			pu+=1
-		elif (c==" "):
-			sp+=1
-	analyzer_prompt(up,lo,pu,sp)
+	uppercase_letters = 0
+	lowercase_letters = 0
+	punctuation_marks = 0
+	spaces = 0
+	for char in text:
+		if char.isalpha():
+			if (char.isupper()):
+				uppercase_letters+=1
+			elif (char.islower()):
+				lowercase_letters+=1
+		elif char in string.punctuation:
+			punctuation_marks+=1
+		elif char.isspace():
+			spaces+=1
+	analyzer_prompt(uppercase_letters,lowercase_letters,punctuation_marks,spaces)
+
+usingError = """
+usingError (too much argument) : python count.py <string>
+"""
+
+if __name__=="__main__":
+	import sys
+
+	if len(sys.argv)>2:
+		print(usingError)
+	elif (len(sys.argv)==2):
+		text_analyzer(sys.argv[1])
