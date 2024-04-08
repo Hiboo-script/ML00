d9058ffeb5bada0ebe900dca2dc64cbad797f5f9
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-04-08 21:22:34 +0200

    ex05 better start code imo, with class implementation

--- ex05/recipe.py
+++ ex05/recipe.py
@@ -1,24 +1,35 @@
-cookbook = dict()
-recipe = dict(ingredients = list(),meal = str(),prep_time = int())
-
-sandwich = recipe.copy()
-sandwich['ingredients'] = ['ham', 'bread', 'cheese', 'tomatoes']
-sandwich['meal'] = 'lunch'
-sandwich['prep_time'] = 10
-
-cake = recipe.copy()
-cake['ingredients'] = ['floor', 'sugar', 'eggs']
-cake['meal'] = 'dessert'
-cake['prep_time'] = 60
-
-salad = recipe.copy()
-salad['ingredients'] = ['avocado', 'arugula', 'tomatoes', 'spinach']
-salad['meal'] = 'lunch'
-salad['prep_time'] = 15 
+class Recipe:
+	def __init__(self, name, ingredients, meal, prep_time):
+		self.name = name
+		self.recipe = dict(ingredients = ingredients, meal = meal,prep_time = prep_time)
+	
+	def getName(self):
+		return self.name
+	
+	def get(self):
+		return self.recipe
 
 
-cookbook['sandwich'] = sandwich
-cookbook['cake'] = cake
-cookbook['salad'] = salad
+class Book:
+	def __init__(self):
+		self.book = dict()
 
-print(cookbook)
+	def addRecipe(self, recipe):
+		self.book[recipe.getName()] = recipe.get()
+
+	def print(self):
+		print(self.book)		
+
+
+cookbook = Book()
+
+sandwich = Recipe('sandwich',['ham', 'bread', 'cheese', 'tomatoes'],'lunch',10)
+cake = Recipe('cake',['floor', 'sugar', 'eggs'],'dessert',60)
+salad = Recipe('salad',['avocado', 'arugula', 'tomatoes', 'spinach'],'lunch', 15)
+
+
+cookbook.addRecipe(sandwich)
+cookbook.addRecipe(cake)
+cookbook.addRecipe(salad)
+
+cookbook.print()
