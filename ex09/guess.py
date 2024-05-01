10fb3e4dca44f3b1140f706852f2f334ea8b90e8
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-01 20:43:46 +0200

    ex09 finish

--- ex09/guess.py
+++ ex09/guess.py
@@ -2,11 +2,14 @@ import random
 
 win_number = random.randint(1,99)
 
-presentation_game = """ """
+presentation_game = """This is an interactive guessing game !
+You have to enter a number between 1 and 99 to find the secret number.
+type 'exit' to end the game.
+good luck!\n"""
 invite_text = 'What\'s your Guess between 1 and 99 ?\n>> '
-entry_42 = ''
+entry_42 = 'The answer to the ultimate question of life, the universe and everything is 42.'
 
-print('presentation game')
+print(presentation_game)
 try_number = input(invite_text)
 number_try = 1
 
@@ -16,8 +19,11 @@ while try_number!='exit':
 		if (try_number == win_number):
 			if (win_number == 42):
 				print(entry_42)
-			print('Congratulation you win in {} attemp(s) !'.format(number_try))
-			print('###### NEW GAME !')
+			if (number_try==1):
+				print('Congratulation! You got it in your first try!')
+			else:
+				print('Congratulation you win in {} attemp(s) !\n'.format(number_try))
+			print('###### NEW GAME ! ########\n')
 			win_number = random.randint(0,99)
 		elif (try_number > win_number):
 			print('Too high !')
