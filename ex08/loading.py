841f4b5aa536125892eceba0e69494e867ebb4a0
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-01 15:40:14 +0200

    ex08 functionnal loading bar ! <3

--- ex08/loading.py
+++ ex08/loading.py
@@ -12,17 +12,22 @@ class ft_progress:
 	def __init__(self,lst):
 		self.lst = lst
 		self.lim = len(lst)
-		self.n = 0
+		self.n = 1
 		self.pourcent = 0 		
 	
 	def __iter__(self):
 		self.a = self.lst[0]
+		self.init_time = time.time()
 		hide_c()
 		return self
 	def __next__(self):
-		if self.n < self.lim:
-			x = self.lst[self.n]
-			print(' [{:0=5.2f}%]'.format(self.actual_pourcent()), end="\r")
+		if self.n <= self.lim:
+			self.actual_time = time.time()
+			x = self.lst[self.n-1]
+			dt_time = self.actual_time-self.init_time
+			es_time = (self.lim/self.n)*dt_time
+			n_bar = int(self.n*(30./self.lim))
+			print('ETA : {:0=4.2f}s [{:0=5.2f}%] [{}>{}] {}/{} | elapsed time : {:0=4.2f}s'.format(es_time,self.actual_pourcent(),'='*n_bar,' '*(30-n_bar), self.n, self.lim, dt_time), end="\r")
 			self.n += 1
 			return x
 		else:
@@ -32,6 +37,7 @@ class ft_progress:
 	def actual_pourcent(self):
 		return ((self.n*1.)/self.lim)*100
 	
+
 listy = range(1000)
 ret = 0
 
